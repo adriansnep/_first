4272314ddd524b541ca728f47509e40b525cf9cf
adriansnep <snep_adi2000@yahoo.com>
2014-10-26 14:29:23 +0200

    second_commit

---
+++ Lab2/src/base_package/mainClass.java
@@ -0,0 +1,20 @@
+package base_package;
+
+/**
+ * this is the main class
+ * @author ADY
+ * @since 2014-10-24
+ */
+public class mainClass {
+
+	/**
+	 * this is the main function from where the program will start execution
+	 * @param args not used for the moment
+	 * @return this function doesn't return anything for the moment
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
